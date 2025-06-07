6b7a410e87693272eb5ba316082796e93e2a258e
julowe <10883713+julowe@users.noreply.github.com>
2025-06-06 21:45:21 -0600

    trying diff way to get notebook config updated

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,6 +4,7 @@ c = get_config()  #noqa
 
 # run it twice so table of contents works
 c.LatexConfig.run_times = 2
+c.LatexConfig.cleanup = False
 
 #------------------------------------------------------------------------------
 # Application(SingletonConfigurable) configuration
